865f687409ac80703b64b4dec4bcb2edca7d7dbf
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-04 20:08:22 -0300

    feat: first commit

---
+++ javaProfessional/challenge_1/challenge1/src/main/java/com/cursoJava/challenge1/Challenge1Application.java
@@ -0,0 +1,18 @@
+package com.cursoJava.challenge1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, world");
+	}
+}
