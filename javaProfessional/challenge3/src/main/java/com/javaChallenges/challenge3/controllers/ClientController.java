7e78604e37b03a3179f66653994c4601798eb11d
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-12 21:18:13 -0300

    feat: clientController created

---
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/controllers/ClientController.java
@@ -0,0 +1,16 @@
+package com.javaChallenges.challenge3.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/clients")
+public class ClientController {
+
+
+
+
+}
