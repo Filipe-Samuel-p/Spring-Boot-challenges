c87cda392c26a87dd68d9540fdc0c34969d7741f
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-12 21:17:57 -0300

    feat: new attributes Added on client class

--- javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/model/Client.java
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/model/Client.java
@@ -2,6 +2,8 @@ package com.javaChallenges.challenge3.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "tb_client")
 public class Client {
@@ -10,5 +12,67 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private String name;
+    private String cpf;
+    private Double income; //(Renda)
+    private LocalDate birthDate;
+    private Integer childrenQuantity;
 
+    public Client (){}
+    public Client(long id, Integer childrenQuantity, String name, String cpf, LocalDate birthDate, Double income) {
+        this.id = id;
+        this.childrenQuantity = childrenQuantity;
+        this.name = name;
+        this.cpf = cpf;
+        this.birthDate = birthDate;
+        this.income = income;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildrenQuantity() {
+        return childrenQuantity;
+    }
+
+    public void setChildrenQuantity(Integer childrenQuantity) {
+        this.childrenQuantity = childrenQuantity;
+    }
 }
