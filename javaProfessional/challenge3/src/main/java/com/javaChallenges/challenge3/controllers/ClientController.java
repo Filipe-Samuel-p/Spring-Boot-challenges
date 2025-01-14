8a601a32c0f1dbc919ece64c7486fb065e4d9631
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-13 21:23:21 -0300

    feat: test function created

--- javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/controllers/ClientController.java
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/controllers/ClientController.java
@@ -1,16 +1,19 @@
 package com.javaChallenges.challenge3.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "/clients")
+@RequestMapping(value = "/clients")
 public class ClientController {
 
-
-
+    @GetMapping
+    public String test(){
+        return "Hello, world";
+    }
 
 }
