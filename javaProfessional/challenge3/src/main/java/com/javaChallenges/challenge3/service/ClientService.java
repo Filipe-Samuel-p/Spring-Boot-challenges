6638c700a683b00ffa1d0e06564867edcdd865ab
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-15 00:10:58 -0300

    refactor: Excpetions Created

--- javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/service/ClientService.java
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/service/ClientService.java
@@ -7,11 +7,11 @@ import com.javaChallenges.challenge3.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -24,6 +24,16 @@ public class ClientService {
 
         Page<Client> allClients = repository.findAll(pageable);
         return allClients.map(client -> new ClientDTO(client.getName(), client.getCpf(), client.getIncome(), client.getBirthDate(), client.getChildrenQuantity()));
+
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+
+        Client clientDataBase = repository.findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT));
+        return new ClientDTO(clientDataBase.getName(), clientDataBase.getCpf(), clientDataBase.getIncome(),
+                clientDataBase.getBirthDate(), clientDataBase.getChildrenQuantity());
 
     }
 
