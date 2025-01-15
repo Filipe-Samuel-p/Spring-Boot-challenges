2fbd20575e5e0ed35514af44c4905814eaa249e1
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-14 23:16:32 -0300

    refactor: findAll method modificated in Controller and service package

--- javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/controllers/ClientController.java
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/controllers/ClientController.java
@@ -1,9 +1,12 @@
 package com.javaChallenges.challenge3.controllers;
 
 
+import com.javaChallenges.challenge3.dto.ClientDTO;
 import com.javaChallenges.challenge3.model.Client;
 import com.javaChallenges.challenge3.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,8 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping
-    public ResponseEntity<List<Client>> findAll(){
-        List<Client> list = service.findAll();
-        return ResponseEntity.ok(list);
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
-
 }
--- javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/service/ClientService.java
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/service/ClientService.java
@@ -1,10 +1,14 @@
 package com.javaChallenges.challenge3.service;
 
 
+import com.javaChallenges.challenge3.dto.ClientDTO;
 import com.javaChallenges.challenge3.model.Client;
 import com.javaChallenges.challenge3.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,9 +19,12 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    public List<Client> findAll(){
-        List<Client> obj = repository.findAll();
-        return obj;
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+
+        Page<Client> allClients = repository.findAll(pageable);
+        return allClients.map(client -> new ClientDTO(client.getName(), client.getCpf(), client.getIncome(), client.getBirthDate(), client.getChildrenQuantity()));
+
     }
 
 }
