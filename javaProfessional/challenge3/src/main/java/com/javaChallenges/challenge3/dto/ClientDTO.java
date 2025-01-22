d920321d52cf3345e8dd0e862e229ecff0cdcc44
Filipe-Samuel-p <felipepires.p125@gmail.com>
2025-01-22 19:01:16 -0300

    refactor: DTO class, no Record

--- javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/dto/ClientDTO.java
+++ javaProfessional/challenge3/src/main/java/com/javaChallenges/challenge3/dto/ClientDTO.java
@@ -2,7 +2,11 @@ package com.javaChallenges.challenge3.dto;
 
 import java.time.LocalDate;
 
-public record ClientDTO(Long id,String name, String cpf,
-                        Double income, LocalDate birthDate, Integer children_quantity) {
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+
+
 
 }
